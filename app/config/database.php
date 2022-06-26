b176000bd6e450fcc22137592a8954efa85d8d2d
gavinb313 <108219572+gavinb313@users.noreply.github.com>
2022-06-26 04:16:34 -0700

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -71,15 +71,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | the query builder class.
 */
 
-//Get Heroku ClearDB connection information
-$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server   = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db       = substr($cleardb_url["path"],1);
-
-
-
 $active_group = 'default';
 $query_builder = TRUE;
 
